0a1673db2dcf04a4b2ef22840897168ec8b29e93
Patrick Naß <patrick.nass@gmx.de>
2013-04-03 18:57:37 +0200

    + added keypress event on username imput field + updated readme

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 WebProgPraktikum
 ================
 
-Repo for the Web-Prog Praktikum: WebQuiz
+The Repository for the WebProgP Project "WebQuiz" was hosted by: Github
+
+Used Technologies for the realization of this project:
+======================================================
+
+Font-Awesome
+http://fortawesome.github.com/Font-Awesome/#icons-web-app
+---------------------------------------------------------
+Repository Technology: Git
+---------------------------------------------------------
+CSS Reset
+http://meyerweb.com/eric/tools/css/reset/
+---------------------------------------------------------
+jQuery
+http://jquery.com/
+---------------------------------------------------------
--- WebContent/loginFeld.js
+++ WebContent/loginFeld.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
-	$("#submitButton").click(function(event){
+	$("#loginButton").click(function(event){
 		if(document.getElementById("nameInput").value.length <= 0)
-			{
+		{
 			alert("Es wurde kein Loginname eingegeben. Bitte versuch es erneut.");
-			}
+		}
 		else
+		{
+			$("#login").hide();
+			$("#content").wrapInner("<table class=\"center\" id=\"loginEingabe\"><td>Bitte w&aumlhle einen Fragekatalog aus.</td></table>");		
+		}
+	});
+	
+	$("#nameInput").bind("keypress", {}, keypressInBox);
+	
+	function keypressInBox(e) {
+		var code = (e.keyCode ? e.keyCode : e.which);
+	    if (code == 13) { //Enter keycode                        
+	        e.preventDefault();
+	        if(document.getElementById("nameInput").value.length <= 0)
+			{
+	        	alert("Es wurde kein Loginname eingegeben. Bitte versuch es erneut.");
+			}
+	        else
 			{
 				$("#login").hide();
-				$("#content").wrapInner('<table class="center" id="loginEingabe"><td>Bitte w&aumlhle einen Fragekatalog aus.</td></table>');
+				$("#content").wrapInner("</table><table class=\"center\" id=\"loginEingabe\"><td>Bitte w&aumlhle einen Fragekatalog aus.</td></table>");
 				
 			}
-	});
+
+	    }
+	}
 });
