5ccb1c25faf6f95441b6224a0bbb416945e1af14
HarryStr <harry.strauch@googlemail.com>
2013-04-27 03:47:40 +0200

    - Client Logik (Fragekatalog laden, Fragen anzeigen, Spielerliste anzeigen) - Spielerliste zu Testzwecken im PlayerServlet eingebaut

--- WebContent/js/loginFeld.js
+++ WebContent/js/loginFeld.js
@@ -1,4 +1,38 @@
 var loginPhase = true;
+var gamePhase = false;
+var startButtonVisible = false;
+var content;
+jQuery.fn.countDown = function(settings,to) {
+	settings = jQuery.extend({
+		startFontSize: "36px",
+		endFontSize: "12px",
+		duration: 1000,
+		startNumber: 10,
+		endNumber: 0,
+		callBack: function() { }
+	}, settings);
+	return this.each(function() {
+		
+		//where do we start?
+		if(!to && to != settings.endNumber) { to = settings.startNumber; }
+		
+		//set the countdown to the starting number
+		jQuery(this).text(to).css("fontSize",settings.startFontSize);
+		
+		//loopage
+		jQuery(this).animate({
+			fontSize: settings.endFontSize
+		}, settings.duration, "", function() {
+			if(to > settings.endNumber + 1) {
+				jQuery(this).css("fontSize", settings.startFontSize).text(to - 1).countDown(settings, to - 1);
+			}
+			else {
+				settings.callBack(this);
+			}
+		});
+				
+	});
+};
 var DatenVomServerVearbeiten = function() {
 	var textfeld= $("#login");
 	switch (request.readyState){
@@ -16,6 +50,80 @@ var DatenVomServerVearbeiten = function() {
 		default: console.log("noch kein open fuer XMLHttpRequest-Objekt erfolgt");
 		}	
 };
+var showQuestion = function(question, answer1,answer2,answer3,answer4,timeout) {
+	content.empty();
+	content.append("<table></table>");
+	var contentTable = $("#content table");
+	contentTable.append("<tr><th>"+question+"</th></tr>");
+	contentTable.append("<tr><td>"+answer1+"</td></tr>");
+	contentTable.append("<tr><td>"+answer2+"</td></tr>");
+	contentTable.append("<tr><td>"+answer3+"</td></tr>");
+	contentTable.append("<tr><td>"+answer4+"</td></tr>");
+	content.append('<div id="countdown"></div>');
+	jQuery("#content #countdown").countDown({
+		startNumber: timeout,
+		callBack: function(me) {
+			jQuery(me).text("");
+		}
+	});
+};
+var startGame = function() {
+	gamePhase = true;
+	loginPhase = false;
+	$.ajax({ 
+	    type: 'POST', 
+	    url: 'CatalogServlet', 
+	    data: { rID: '7' , filename: $(".catList .selected").text() }, 
+	    dataType: 'json',
+	    success: function (data) { 
+	        $.each(data, function(index, element) {
+	        	
+	        });
+	    }
+	});
+	var question ="Ein Thread soll auf ein durch einen anderen Thread ausgelöstes Ereignis warten. Welcher Mechanismus ist geeignet?";
+	var answer1 = "nur Semaphore";
+	var answer2 = "nur Mutex";
+	var answer3 = "weder Semaphore noch Mutexe";
+	var answer4 = "sowohl Semaphore als auch Mutexe";
+	var timeout = 30;
+
+	showQuestion(question, answer1,answer2,answer3,answer4,timeout);
+};
+var initGameStartButton = function () {
+	var button = '<input class="startButton" type="button" name="Text 2" value="Spiel starten"></input>';
+	content.append(button);
+	startButtonVisible = true;
+	$("#content .startButton").click(function(e) {
+		startGame();
+	});
+};
+var initCatalogList = function () {
+	var catElements = $(".catList");
+	catElements.children().each(function() {   
+		$(this).click(function(event){
+			if(gamePhase==false) {
+				$.ajax({ 
+				    type: 'POST', 
+				    url: 'CatalogServlet', 
+				    data: { rID: '5' , filename: $(this).text() }, 
+				    dataType: 'json',
+				    success: function (data) { 
+				        $.each(data, function(index, element) {
+				        	
+				        });
+				    }
+				});
+		    	$(".catList .selected").removeClass("selected");
+		    	$(this).addClass("selected");
+		    	if(startButtonVisible==false){
+		    		initGameStartButton();
+		    	}
+			}
+		});
+	});
+};
+
 var loggedIn = function (e) {
 	if(loginPhase==true){
 		if(document.getElementById("nameInput").value.length <= 0){
@@ -23,40 +131,50 @@ var loggedIn = function (e) {
 		}
 		else {
 			$("#login").hide();
-			$("#content").wrapInner("<table class=\"center\" id=\"loginEingabe\"><td>Bitte w&aumlhle einen Fragekatalog aus.</td></table>");		
+			content.wrapInner("<table class=\"center\" id=\"loginEingabe\"><td>Bitte w&aumlhle einen Fragekatalog aus.</td></table>");		
 		}
 		loginPhase=false;
 	}
 };
-var send = function (event,ElemDesc,ElemVal) {
-	//var button = event.target;
-	request = new XMLHttpRequest();
-	request.onreadystatechange = DatenVomServerVearbeiten;
-	request.open("POST","CatalogServlet",true);
-	request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	//console.log(getElem.val());
-	request.send(""+ElemDesc+"="+ElemVal+"");
-};
-//var showCatalogNames = function () {
-//	var catArr = new Array();
-//	catArr[0] = "one.cat";
-//	catArr[1] = "simple.cat";
-//	catArr[2] = "Systemprogrammierung.cat";
-//	for (var i = catArr.length-1;i>=0 ;i--){
-//		$(".catList").append("<li>"+catArr[i]+"</li>");
+//var send = function (event, ElemDesc,ElemVal) {
+//	//var button = event.target;
+//	request = new XMLHttpRequest();
+//	request.onreadystatechange = DatenVomServerVearbeiten;
+//	request.open("POST","CatalogServlet",true);
+//	request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+//	//console.log(getElem.val());
+//	var requestString = "";
+//	var length = arr.length;
+//	for (var i = 0; i < length; i++) {
+//		requestString += ""+ElemDesc[i]+"="+ElemVal[i]+"";
+//	  // Do something with element i.
 //	}
-//	console.log(catArr);
+//	request.send(requestString);
 //};
+
 var loginURL = "localhost:8080/WebQuiz/LogicServlet";
 var ws = new WebSocket("ws://"+loginURL);
 
 $(document).ready(function() {
-	
+	content = $("#content");
 	console.log("Content geladen");
-	//showCatalogNames();
 
 	$("#loginButton").click(function(event){
-		send(event,"name",$("#nameInput").val());
+		//send(event,"name",$("#nameInput").val());
+		
+		
+		$.ajax({ 
+		    type: 'POST', 
+		    url: 'PlayerServlet', 
+		    data: { rID: '1' , name: $("#nameInput").val() }, 
+		    dataType: 'json',
+		    success: function (data) { 
+		        $.each(data, function(index, element) {
+		        	loggedIn();
+		        	$("#highscore table tbody").append("<tr><td>"+element+"</td><td>0</td></tr>");
+		        });
+		    }
+		});
 		
 		$.ajax({ 
 		    type: 'POST', 
@@ -67,14 +185,10 @@ $(document).ready(function() {
 		        $.each(data, function(index, element) {
 		        	$(".catList").append("<li>"+element.name+"</li>");
 		        });
+		        initCatalogList();
 		    }
 		});
-		var catElements = $(".catList");
-		catElements.children().each(function() {   
-			$(this).click(function(event){
-				send(event,"catalog",$(this).text()); 
-			});
-		});
+
 		
 		ws.onopen = function(){
         };
--- src/de/quiz/Servlets/PlayerServlet.java
+++ src/de/quiz/Servlets/PlayerServlet.java
@@ -2,6 +2,9 @@ package de.quiz.Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,9 +15,11 @@ import javax.servlet.http.HttpSession;
 
 import org.json.JSONObject;
 
+import de.fhwgt.quiz.application.Catalog;
 import de.fhwgt.quiz.application.Player;
 import de.fhwgt.quiz.application.Quiz;
 import de.fhwgt.quiz.error.QuizError;
+import de.fhwgt.quiz.loader.LoaderException;
 import de.quiz.LoggingManager.ILoggingManager;
 import de.quiz.ServiceManager.ServiceManager;
 import de.quiz.UserManager.IUserManager;
@@ -25,12 +30,13 @@ import de.quiz.UserManager.IUserManager;
 @WebServlet(description = "handles everything which has to do with players", urlPatterns = { "/PlayerServlet" })
 public class PlayerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    private ArrayList<String> PlayerList;
     /**
      * @see HttpServlet#HttpServlet()
      */
     public PlayerServlet() {
         super();
+        this.PlayerList = new ArrayList<String>();
         // TODO Auto-generated constructor stub
     }
 
@@ -57,15 +63,27 @@ public class PlayerServlet extends HttpServlet {
 			long userID;
 			try {
 				userID = ServiceManager.getInstance().getService(IUserManager.class).loginUser(request.getParameter("name"), session);
-				out.print(userID);
+				//out.print(userID);
+				this.PlayerList.add(request.getParameter("name"));
 				ServiceManager.getInstance().getService(ILoggingManager.class).log("Successfully logged in User with ID: "+userID);;
 			} catch (Exception e) {
 				ServiceManager.getInstance().getService(ILoggingManager.class).log("User login failed!");
 			}
-
+			//Hier Playerlist senden
+				response.setContentType("application/json");
+				JSONObject json = new JSONObject(this.getPlayerList());
+				out.print(json);
 			//ServiceManager.getInstance().getService(ILoggingManager.class).log("Send Playerthings");
 		}
 	}
-
+	protected Map<String, String> getPlayerList() {
+		Map<String, String> playerMap = new HashMap<String, String>();
+		int i = 0;
+		for (String player : this.PlayerList) {
+			playerMap.put("name"+i, player);
+			i++;
+		}
+		return playerMap;
+	}
 
 }
