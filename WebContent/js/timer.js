cdcb11cf35b553300e62e3719cae3d41572ff567
René Blänsdorf <KonStudieAI@gmail.com>
2013-05-05 16:01:34 +0200

    Should now send index of the selected answer to the LogicServlet. Answers are disabled after clicking one. Timer shows the time that was left.

--- WebContent/js/timer.js
+++ WebContent/js/timer.js
@@ -1,32 +1,46 @@
 /* CountdownTimer Funktion */
-jQuery.fn.countDown = function(settings,to) {
+var stopCountdown = false;
+
+jQuery.fn.countDown = function(settings, to) {
 	settings = jQuery.extend({
-		startFontSize: "36px",
-		endFontSize: "12px",
-		duration: 1000,
-		startNumber: 10,
-		endNumber: 0,
-		callBack: function() { }
+		startFontSize : "36px",
+		endFontSize : "12px",
+		duration : 1000,
+		startNumber : 10,
+		endNumber : 0,
+		callBack : function() {
+		}
 	}, settings);
 	return this.each(function() {
-		
-		//where do we start?
-		if(!to && to != settings.endNumber) { to = settings.startNumber; }
-		
-		//set the countdown to the starting number
-		jQuery(this).text(to).css("fontSize",settings.startFontSize);
-		
-		//loopage
-		jQuery(this).animate({
-			fontSize: settings.endFontSize
-		}, settings.duration, "", function() {
-			if(to > settings.endNumber + 1) {
-				jQuery(this).css("fontSize", settings.startFontSize).text(to - 1).countDown(settings, to - 1);
+		if (stopCountdown == false) {
+
+			// where do we start?
+			if (!to && to != settings.endNumber) {
+				to = settings.startNumber;
 			}
-			else {
-				settings.callBack(this);
-			}
-		});
-				
+
+			// set the countdown to the starting number
+			jQuery(this).text(to).css("fontSize", settings.startFontSize);
+
+			// loopage
+			jQuery(this).animate(
+					{
+						fontSize : settings.endFontSize
+					},
+					settings.duration,
+					"",
+					function() {
+						if (stopCountdown == false) {
+							if (to > settings.endNumber + 1) {
+								jQuery(this).css("fontSize",
+										settings.startFontSize).text(to - 1)
+										.countDown(settings, to - 1);
+							} else {
+								settings.callBack(this);
+							}
+						}
+					});
+		}
+
 	});
 };
