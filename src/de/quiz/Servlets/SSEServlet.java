f8bba3c79ba6cb347a9e989986555f84adb694e2
Chevapchichi <harry.strauch@googlemail.com>
2013-06-17 12:41:04 +0200

    kleine anpassung am sse

--- src/de/quiz/Servlets/SSEServlet.java
+++ src/de/quiz/Servlets/SSEServlet.java
@@ -30,6 +30,7 @@ import de.quiz.LoggingManager.ILoggingManager;
 import de.quiz.ServiceManager.IService;
 import de.quiz.ServiceManager.ServiceManager;
 import de.quiz.User.IUser;
+import de.quiz.User.User;
 import de.quiz.UserManager.IUserManager;
 
 /**
@@ -38,7 +39,7 @@ import de.quiz.UserManager.IUserManager;
 @WebServlet(description = "handles everything which has to do with players", urlPatterns = { "/SSEServlet" })
 public class SSEServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static CopyOnWriteArrayList<ClientConnection> clientConArr = new CopyOnWriteArrayList<ClientConnection>();
+	private static CopyOnWriteArrayList<IUser> userArr = new CopyOnWriteArrayList<IUser>();
 	private static CopyOnWriteArrayList<AsyncContext> asyncArr = new CopyOnWriteArrayList<AsyncContext>();
 
 	// private static CopyOnWriteArrayList<AsyncContext> asyncArr = new
@@ -55,26 +56,26 @@ public class SSEServlet extends HttpServlet {
 			throws ServletException, IOException {
 		//bradcast
 		
-		addClientConnection(req,res);
+		addIUser(req,res);
 		System.out.println("Start broadcast");
-		for (ClientConnection clientCon : clientConArr) {
-			sendMsg(clientCon,6);
+		for (IUser user : userArr) {
+			sendMsg(user,6);
 		}
 	}
 
-	public static void sendMsg(final ClientConnection clientCon , int msg)
+	public static void sendMsg(final IUser user , int msg)
 			throws ServletException, IOException {
 		// create the async context, otherwise getAsyncContext() will be
 		// null
-		HttpServletRequest req = clientCon.getRequest();
-		HttpServletResponse res = clientCon.getResponse();
+		HttpServletRequest req = user.getRequest();
+		HttpServletResponse res = user.getResponse();
 		PrintWriter out = res.getWriter();
 		System.out.println("bla: "+out);
 		System.out.println("response status: "+res.getStatus());
 		final AsyncContext ctx;
 		if(req.getAsyncContext()==null){
 			ctx = req.startAsync();
-			ctx.setTimeout(10000);
+			ctx.setTimeout(0);
 		}else{
 			ctx = req.getAsyncContext();
 		}
@@ -91,7 +92,7 @@ public class SSEServlet extends HttpServlet {
 			public void onTimeout(AsyncEvent event) throws IOException {
 				System.out.println("onTimeout called");
 				//ctx.complete();
-				clientConArr.remove(clientCon);
+				userArr.remove(user);
 				
 			}
 
@@ -111,22 +112,23 @@ public class SSEServlet extends HttpServlet {
 		ctx.start(clientThread);
 
 	}
-	public static boolean addClientConnection(HttpServletRequest request, HttpServletResponse response) {
-		for (ClientConnection clientCon : clientConArr) {
-			if (clientCon.getRequest() == request) {
+	public static boolean addIUser(HttpServletRequest request, HttpServletResponse response) {
+		for (IUser user : userArr) {
+			if (user.getRequest() == request) {
 				System.out.println("fehler");
 				return false;
 			}
 		}
-		ClientConnection clientCon = new ClientConnection(0, request,
-				response);
-		clientConArr.add(clientCon);
+		IUser user = new User();
+		user.setRequest(request);
+		user.setResponse(response);
+		userArr.add(user);
 		return true;
 	}
 	public static boolean broadcast(int msg) throws ServletException, IOException {
 		System.out.println("Start broadcast");
-		for (ClientConnection clientCon : clientConArr) {
-			sendMsg(clientCon,msg);
+		for (IUser user : userArr) {
+			sendMsg(user,msg);
 		}
 		return true;
 	}
@@ -147,19 +149,19 @@ public class SSEServlet extends HttpServlet {
  * 
  * public static void broadcast(final int messageId){
  * System.out.println("Start Broadcast: "+messageId); for (final
- * ClientConnection clientCon : clientConArr) { if (clientCon.getRequest()!=
- * null && clientCon.getResponse()!=null){ clientCon.getAsyncCo().notify(); } }
- * } public static boolean addClientConnection(int clientId, HttpServletRequest
- * request, HttpServletResponse response) { for (ClientConnection clientCon :
- * clientConArr) { if(clientCon.getRequest()==request){
- * System.out.println("fehler"); return false; } } ClientConnection clientCon =
- * new ClientConnection(clientId,request,response); clientConArr.add(clientCon);
- * return true; } public static boolean removeClientConnectionById(int clientId)
- * { for (ClientConnection clientCon : clientConArr) {
- * if(clientCon.getClientId()==clientId){ clientConArr.remove(clientCon); return
+ * IUser user : userArr) { if (user.getRequest()!=
+ * null && user.getResponse()!=null){ user.getAsyncCo().notify(); } }
+ * } public static boolean addIUser(int clientId, HttpServletRequest
+ * request, HttpServletResponse response) { for (IUser user :
+ * userArr) { if(user.getRequest()==request){
+ * System.out.println("fehler"); return false; } } IUser user =
+ * new IUser(clientId,request,response); userArr.add(user);
+ * return true; } public static boolean removeIUserById(int clientId)
+ * { for (IUser user : userArr) {
+ * if(user.getClientId()==clientId){ userArr.remove(user); return
  * true; } } return false; } public static boolean
- * removeClientConnectionByRequest(HttpServletRequest request) { for
- * (ClientConnection clientCon : clientConArr) {
- * if(clientCon.getRequest()==request){ clientConArr.remove(clientCon); return
+ * removeIUserByRequest(HttpServletRequest request) { for
+ * (IUser user : userArr) {
+ * if(user.getRequest()==request){ userArr.remove(user); return
  * true; } } return false; } }
  */
