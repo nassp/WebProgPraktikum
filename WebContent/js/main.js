9c80aba7842c7f90ef9087d76cc05634e4c1c232
Harry Strauch <harry.strauch@googlemail.com>
2013-06-28 00:45:53 +0200

    small fix

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -5,9 +5,9 @@ var catalogSelected = false;
 var content;
 var userId = -1;
 //Websocket
-var siteURL ='http://localhost:8080/WebQuiz/';
+var siteURL = 'localhost:8080/WebQuiz/';
 var loginURL = siteURL+'LogicServlet';
-var sseURL = siteURL+'SSEServlet';
+var sseURL = 'http://'+siteURL+'SSEServlet';
 var ws;
 var acceptAnswer = true;
 var moreThan2 = false;
