ef81450d9133a90413673b313dc339b6cff0d819
René Blänsdorf <KonStudieAI@gmail.com>
2013-06-28 00:14:47 +0200

    Forgot doxygen comments at the timeout class

--- src/de/quiz/Utility/TimeOut.java
+++ src/de/quiz/Utility/TimeOut.java
@@ -3,14 +3,19 @@ package de.quiz.Utility;
 import java.io.IOException;
 import java.nio.CharBuffer;
 import java.util.TimerTask;
-
 import org.apache.catalina.websocket.WsOutbound;
-
 import de.fhwgt.quiz.application.Player;
 import de.fhwgt.quiz.application.Question;
 import de.fhwgt.quiz.application.Quiz;
 import de.fhwgt.quiz.error.QuizError;
 
+
+/**
+ * TimeOut implementing TimerTask for the gameLogic Timeout
+ * Defines what happens after the timeout is reached
+ * 
+ * @see TimerTask#TimerTask()
+ */
 public class TimeOut extends TimerTask {
 
 	private WsOutbound myOutbound = null;
@@ -18,20 +23,41 @@ public class TimeOut extends TimerTask {
 	long index = 10;
 	Question quest;
 
-	// Constructor with the outbound and player to write messages
+	/**
+	 * constructor which sets the outbound and player to be able to write messages
+	 * 
+	 * @param outbound
+	 * @param p
+	 *            the player the timeout belongs to
+	 */
 	public TimeOut(WsOutbound outbound, Player p) {
 		myOutbound = outbound;
 		this.p = p;
 	}
 
+	/**
+	 * sets the index of the right answer
+	 * 
+	 * @param index
+	 *            index if the right answer
+	 */
 	public void setIndex(long index) {
 		this.index = index;
 	}
 
+	/**
+	 * sets the question the timeout belongs to
+	 * 
+	 * @param quest
+	 *            question the timeout belongs to
+	 */
 	public void setThisQuestion(Question quest) {
 		this.quest = quest;
 	}
 
+	/**
+	 * @see Runnable#run()
+	 */
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
